b5af35a9765236b9a4ec0efd5017ffc6916a0538
Evgeny Balashov <ebalashov@networkoptix.com>
2020-04-05 17:13:18 -0700

    Vote logic

--- backend/mafia.js
+++ backend/mafia.js
@@ -4,15 +4,13 @@
 // Later for addional roles - check here: https://summoning.ru/games/mafia.shtml
 // or here: https://en.wikipedia.org/wiki/Mafia_(party_game)
 //
-// Option: dead disclose their role or not
+// Option: dead disclose their role or not (default - no)
 // Option: mafia doesn't talk (in original everyone has to write down "I'm civilian", or name of the player to eliminate). This way prevents cheating as no one has to close their eyes
 // Option: First dead becomes game host (downside - not everyone is a good host)
+// Option: dead see who is who
+// Option: vote is mandatory (default - yes) - implemented
 // Idea: we could make our tool talk to players to voice commands
-// TODO: implement voting for mafia and maybe daytime voting
-// TODO: check if special night logic is required
-
-exports.Game = MafiaGame;
-exports.Roles = MafiaRoles;
+// TODO: implement day-night state
 
 /*
   mafia should have 1/3 or less of players
@@ -48,103 +46,180 @@ const MafiaRoles = Object.freeze({
   Maybe we should introduce new game roles for larger rooms
  */
 const CardsDeck = [
-      Roles.Mafia,
-      Roles.Mafia,
-      Roles.Civilian,
-      Roles.Civilian,
-      Roles.Civilian,
-      Roles.Civilian,  // 6 players ends here
-      Roles.Detective, // 7 players
-      Roles.Mafia,     // 8 players
-      Roles.Civilian,  // 9 players
-      Roles.Civilian,  // 10 players
-      Roles.Don,       // 11 players
-      Roles.Civilian,  // 12 players
-      Roles.Civilian,  // 13 players
-      Roles.Mafia,     // 14 players
-      Roles.Civilian,  // 15 players
-      Roles.Civilian,  // 16 players
-      Roles.Master,      // 17 players - too much, someone gets to be a host
+      MafiaRoles.Mafia,
+      MafiaRoles.Mafia,
+      MafiaRoles.Civilian,
+      MafiaRoles.Civilian,
+      MafiaRoles.Civilian,
+      MafiaRoles.Civilian,  // 6 players ends here
+      MafiaRoles.Detective, // 7 players
+      MafiaRoles.Mafia,     // 8 players
+      MafiaRoles.Civilian,  // 9 players
+      MafiaRoles.Civilian,  // 10 players
+      MafiaRoles.Don,       // 11 players
+      MafiaRoles.Civilian,  // 12 players
+      MafiaRoles.Civilian,  // 13 players
+      MafiaRoles.Mafia,     // 14 players
+      MafiaRoles.Civilian,  // 15 players
+      MafiaRoles.Civilian,  // 16 players
+      MafiaRoles.Master,      // 17 players - too much, someone gets to be a host
 
       // Now, here I'm too lazy to think, so I just add a bunch of guests here:
-      Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest,
-      Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest,
-      Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest,
-      Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest, Roles.Guest
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest,
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest,
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest,
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest,
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest,
+      MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest, MafiaRoles.Guest
     ];
 
 class MafiaGame {
-  constructor(){
+  constructor(isVoteMandatory = true, // Is everyone must vote (unresolved votes go for the first player on the voting list)
+              isMafiaVoteUnanimous = false) // Should mafia vote by unanimous (professional rules)
+  {
+    this.isVoteMandatory = isVoteMandatory;
+    this.isMafiaVoteUnanimous = isMafiaVoteUnanimous
   },
 
   start(playersNames){
     //This method starts new game based on the array of player names
 
-    var playersRoles = this._shuffle(players.length); // Shuffle the roles
+    let playersRoles = this._shuffle(players.length); // Shuffle the roles
 
     // Generate states for everyPlayer
-    this.players = playersNames.map(function callback(element, index, array) {
+    this.players = playersNames.map(function callback(element, index) {
         // Return value for new_array
         return {
           number: index + 1,
           name: element,
           role: playersRoles[index],
-          isMafia: _isMafia(playersRoles[index]),
-          alive: true
+          isMafia: this._isMafiaRole(playersRoles[index]),
+          isAlive: this._isActiveRole(playersRoles[index]) // mark guests and master as dead - to prevent from voting
         }
     });
     this.gameOn = true;
   },
 
-  _isMafia(role){
-    return (role === Roles.Mafia) || (role === Roles.Don);
+  static _isMafiaRole(role){
+    return (role === MafiaRoles.Mafia) || (role === MafiaRoles.Don);
+  },
+  static _isActiveRole(role){
+    return role > 1;
   },
 
-  _shuffle(numberOfCards){
+  static _shuffle(numberOfCards){
     // This method generates an array of roles based on number of players
-    var cardsToPlay = CardsDeck.slice(numberOfCards);
+    let cardsToPlay = CardsDeck.slice(numberOfCards);
     cardsToPlay.sort(() => Math.random() - 0.5); // Shuffle the array, solution from here: https://javascript.info/task/shuffle
     return cardsToPlay;
   },
 
   kill(playerNumber){
-    this.players[playerNumber-1].alive = false;
+    this.players[playerNumber-1].isAlive = false;
     this.checkGameOver(); // Check if game is over with every kill
     return false;
   },
 
-  voteToKill(whoVotes, choicePlayer, checkMafia){
 
+  startVote(mafiaOnly){
+    /* There are three votes in the game:
+    1) Daytime - who are suspects (who shall we nominate for killing)
+    2) Daytime - who is guilty (who shall be killed)
+    3) Nighttime - mafia only - who to eliminate
+     */
+    // TODO: remember and check who should be voted for in scenario 2
+    this.votes = {};
+    this.mafiaVotes = mafiaOnly;
   },
+  shouldVote(player){
+    return player.isAlive && (!this.mafiaVotes || player.isMafia);
+  },
+
+  vote(whoVotes, choicePlayer){
+    if(!this.shouldVote(this.players[whoVotes-1].isAlive)){
+      return; //This vote doesn't count
+    }
+
+    this.votes[whoVotes] = choicePlayer; // Using array to have unique vote per player
+    if(this.checkAllVoted()){
+      this.resolveVote();
+    }
+  },
+  showShouldVote(){
+    return this.players.filter(player => this.shouldVote(player));
+  },
+  checkAllVoted(){
+    // For automatic vote resolve - once everyone votes
+    return !this.showShouldVote().some(player => !this.votes[player.number]);
+  },
+  resolveVote(){
+    // Didn't vote - vote goes to the first of the list
+    let unusedVotesCounter = 0;
+
+    if(this.isVoteMandatory || this.isMafiaVoteUnanimous){
+      unusedVotesCounter = this.showShouldVote().find(player => !this.votes[player.number]).length;
+    }
+
+
+    // Count votes
+    let votedDown = {};
+    this.votes.values().forEach(vote => {
+      if(!votedDown[vote]){
+        votedDown[vote] = 0;
+      }
+      votedDown[vote] ++;
+    });
+
+    let votes = votedDown.entries();
+
+    if(this.mafiaVotes && this.isMafiaVoteUnanimous){
+      if(unusedVotesCounter > 0 || votes.length > 1){
+        return false;
+      }
+    }
+
+    if(this.isVoteMandatory && !this.mafiaVotes){
+      // Add all unused votes to the smallest player number
+      votes.sort(element => element[0]);
+      votes[0][1] += unusedVotesCounter;
+    }
+
+    votes.sort(element => -element[1]); // Sort by votes in reverse order
+    return votes[0][0]; // return the first voted
+  },
+
 
   checkGameOver(){
     // Game over conditions:
     // MafiaWins: number of alive mafia is >= number of alive civilians
     // Civilians win: no mafia is alive
 
-     var mafiaCount = this.players.reduce(function (result, player) {
+     let mafiaCount = this.players.reduce(function (result, player) {
         return result + ((player.isAlive && player.isMafia) ?1:0);
      }, 0);
 
-     if(mafiaCount == 0){
+     if(mafiaCount === 0){
        this.gameOn = false;
-       this.civilianWin = true;
+       this.civiliansWin = true;
        return true;
      }
 
-     var civilianCount = this.players.reduce(function (result, player) {
+     let civilianCount = this.players.reduce(function (result, player) {
         return result + ((player.isAlive && !player.isMafia) ?1:0);
      }, 0);
 
 
      if(mafiaCount >= civilianCount){
        this.gameOn = false;
-       this.civilianWin = false;
+       this.civiliansWin = false;
        return true;
      }
 
      return false;
   }
 }
+
+exports.Game = MafiaGame;
+exports.Roles = MafiaRoles;
 
 
