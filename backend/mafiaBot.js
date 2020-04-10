2a7936a1ff23fc69bff757fa8dd7250e9e14c989
Evgeny Balashov <ebalashov@networkoptix.com>
2020-04-10 02:13:49 -0700

    trying to add bot

---
+++ backend/mafiaBot.js
@@ -0,0 +1,73 @@
+
+const io = require('socket.io-client');
+
+class MafiaBot {
+  constructor(server, roomId, i){
+    console.log("init bot");
+    this.roomId = roomId;
+    this.id = "Bot " + i;
+    this.name = "Bot " + i;
+
+    this.isOnline= true;
+    this.socket = io(server, {
+      forceNew: true,
+      autoConnect: false,
+      query: {
+      // Pass parameters to join the room automatically
+        id: roomId,
+        userName: this.name,
+        userId: this.id
+      }
+    }); // Open connection
+
+
+    //console.log("init bot", this.socket);
+
+    // Log all messages
+    this.socket.on("roomEvent", data =>{
+      console.log(">> roomEvent", data);
+    });
+
+    this.socket.on("gameEvent", data =>{
+      console.log(">> gameEvent", data);
+    });
+    this.socket.on("directMessage", data =>{
+      console.log(">> gameEvent", data);
+    });
+
+
+    this.socket.on("connect", data => this.log("error", data));
+    this.socket.on("connection", data => this.log("error", data));
+    this.socket.on("error", data => this.log("error", data));
+    this.socket.on("connect_error", data => this.log("connect_error", data));
+    this.socket.on("connect_timeout", data => this.log("connect_timeout", data));
+    this.socket.on("pong", data => this.log("pong", data));
+    this.socket.on("ping", data => this.log("ping", data));
+
+
+    // Vote randomly
+
+
+    this.socket.on("disconnect", data =>{
+      console.log("disconnect", data);
+      this.isOnline = false;
+    });
+
+    console.log("trying to connect");
+    this.socket.connect(data => this.log("ping", data));
+
+    console.log("kinda done");
+  }
+
+  log(event, data){
+      console.log(event, data);
+  }
+  isAlive(){
+    return this.isOnline;
+  }
+
+}
+
+
+
+exports.Bot = MafiaBot;
---
+++ bots.js
@@ -0,0 +1,43 @@
+// This script spawns bot to join a game
+
+const { Command } = require('commander');
+
+const { Bot } = require("./backend/mafiaBot");
+
+const DEFAULT_BOTS_NUMBER = 10;
+const DEFAULT_ROOM = 'bots';
+const DEFAULT_SERVER = 'ws://127.0.0.1:8080';
+
+const program = new Command();
+program.version('0.0.1');
+
+program
+  .option('-n, --number <number>', 'number of bots to spawn')
+  .option('-r, --room <roomId>', 'name of the room to join')
+  .option('-s, --server <IP:port>', 'server to join');
+
+program.parse(process.argv);
+
+let botsNumber = program.number || DEFAULT_BOTS_NUMBER;
+let roomId = program.room || DEFAULT_ROOM;
+let server = program.server || DEFAULT_SERVER;
+
+console.log("bots", botsNumber, roomId, server);
+
+let bots = [];
+for(let i=0;i<botsNumber;i++){
+  let bot = new Bot(server, roomId, i);
+  bots.push(bot);
+}
+
+let isAlive = false;
+
+do{
+
+  for(let i=0;i<bots.length;i++){
+    isAlive = isAlive || bots[i].isAlive();
+  }
+} while(isAlive);
+
+
+console.log("bots are dead");
