98cf13142c5f82d7f504a38dd6b3b1298573d18d
Evgeny Balashov <ebalashov@networkoptix.com>
2020-04-15 00:55:23 -0700

    fix detective investigation at day

--- backend/mafia.js
+++ backend/mafia.js
@@ -655,7 +655,7 @@ class MafiaGame {
     let alreadyVoted = !!this.votesRegistry[whoVotes];
     this.votesRegistry[whoVotes] = choicePlayer; // Using array to have unique vote per player
 
-    if(!alreadyVoted && player.role === MafiaRoles.Detective){
+    if(!alreadyVoted && player.role === MafiaRoles.Detective && this.gameState === GameStates.Night){
       // this is a detecitve. we remember that he knows about this player now, with next game update he will get information
       this.detectiveKnows.push(choicePlayer);
     }
